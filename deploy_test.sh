203cd5edeb4bc13ca6ef745a4f2798d8a724718d
Ivan Robles <irae45@gmail.com>
2023-08-08 15:29:36 -0600

    fix bash script.

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Deploy Test"
+node_env=$1
+port=$2
+db_user=$3
+db_password=$4
+db_host=$5
+db_name=$6
+db_port=$7
+
+echo -e "NODE_ENV=$node_env\nPORT=$port\nDB_USER=$db_user\nDB_PASSWORD=$db_password\nDB_HOST=$db_host\nDB_NAME=$db_name\nDB_PORT=$db_port" > .env
