fb11470986e95b3cbb2a42cb4a3c8b9f150ab136
Ivan Robles <irae45@gmail.com>
2023-08-09 23:22:13 -0600

    add script to deploy in case of there is no project running.

--- deploy_test.sh
+++ deploy_test.sh
@@ -19,12 +19,12 @@ if [[ -d pathMainDir ]]; then
   git pull origin main
   echo "Deployment succesful"
   npm install
-  echo -e "NODE_ENV=$node_env\nPORT=$port\nDB_USER=$db_user\nDB_PASSWORD=$db_password\nDB_HOST=$db_host\nDB_NAME=$db_name\nDB_PORT=$db_port" > .env
+  echo -e "NODE_ENV=$node_env\nPORT=$port\nDB_USER=$db_user\nDB_PASSWORD=$db_password\nDB_HOST=$db_host\nDB_NAME=$db_name\nDB_PORT=$db_port" >.env
   pm2 start index.js --name deploy-test
   if [[ migrations -gt $(find /var/www/test/deploy-test/src/db/migrations | wc -l) ]]; then
     npm run migrations:run
   fi
-  else
+else
   echo "Directory not found"
   mkdir test
   cd test || exit
@@ -32,9 +32,8 @@ if [[ -d pathMainDir ]]; then
   echo "Deployment succesful"
   cd deploy-test || exit
   npm install
-  echo -e "NODE_ENV=$node_env\nPORT=$port\nDB_USER=$db_user\nDB_PASSWORD=$db_password\nDB_HOST=$db_host\nDB_NAME=$db_name\nDB_PORT=$db_port" > .env
+  echo -e "NODE_ENV=$node_env\nPORT=$port\nDB_USER=$db_user\nDB_PASSWORD=$db_password\nDB_HOST=$db_host\nDB_NAME=$db_name\nDB_PORT=$db_port" >.env
   pm2 start index.js --name deploy-test
   npm run migrations:run
   npm run migrations:run:seed
 fi
-
